f2f3d4eb80d7e20784cb021ae8a6e957bf12bd34
giulianaflores <lucassamuelreis.amaral@gmail.com>
2022-01-14 15:59:46 -0300

    Simple Create form

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using SalesWebMvc.Models;
 
+
 namespace SalesWebMvc.Services
 {
     public class SellerService
@@ -19,5 +20,12 @@ namespace SalesWebMvc.Services
         {
             return _context.Seller.ToList();
         }
+        public void Insert(Seller obj) 
+        {
+            _context.Add(obj);
+            _context.SaveChanges();
+        }
+
+
     }
 }
